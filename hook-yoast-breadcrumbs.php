14c8a7586421e095b46c43bd431914f52a746b13
Iakovos Frountas <iakovosfrountas@gmail.com>
2017-01-02 11:53:13 +0200

    Add new file
    Add new file to hook yoast breadcrumbs

---
+++ hook-yoast-breadcrumbs.php
@@ -0,0 +1,19 @@
+<?php
+
+
+    //Remove or add anything you want from Yoast Breadcrumb structure
+    
+    function hook_your_breadcrumb( $links ) {
+        global $post;
+
+        if ( is_home() || is_singular( 'YOUR_POST_TYPE_GOES_HERE' ) || is_archive() ) {
+
+            array_splice( $links, 1, 1, '' ); // Leave the last field blank will remove the link in position 1 of breadcrumb
+        }
+
+        return $links;
+    }
+
+    add_filter( 'wpseo_breadcrumb_links', 'hook_your_breadcrumb' );
+
+?>
